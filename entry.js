9bb42454a44df98f78b4e80e1cef45cbacd6439b
Guy Hadas <GuyHadas195@gmail.com>
2016-05-26 09:23:39 -0700

    initial commit

--- entry.js
+++
@@ -1,6 +0,0 @@
-var GameView = require("./gameView.js");
-var key = require("./keymaster.js");
-
-var ctx = document.getElementById("game-canvas").getContext("2d");
-var gv = new GameView(ctx);
-gv.start();
